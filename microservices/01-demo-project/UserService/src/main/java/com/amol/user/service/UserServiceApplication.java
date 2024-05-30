6559e392549041c61269c886e40a6358969b48d7
Amol-RG001 <gahaneamol37@gmail.com>
2024-05-30 22:49:13 +0530

    added client - eureka service registry as a client

--- microservices/01-demo-project/UserService/src/main/java/com/amol/user/service/UserServiceApplication.java
+++ microservices/01-demo-project/UserService/src/main/java/com/amol/user/service/UserServiceApplication.java
@@ -3,6 +3,8 @@ package com.amol.user.service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//No need to mark @EnableEurekaClient since it's a spring-boot 3.x
+//itself consider eureka client as we added dependencies
 @SpringBootApplication
 public class UserServiceApplication {
 
