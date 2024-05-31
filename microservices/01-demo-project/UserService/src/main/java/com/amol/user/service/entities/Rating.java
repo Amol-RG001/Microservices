414ddc768d9294fdbe64a022f3462d48a2aadd82
Amol-RG001 <gahaneamol37@gmail.com>
2024-05-31 19:03:17 +0530

    fixed arrayList bug and added feature user-service communicate with hotel-service and rating-service

--- microservices/01-demo-project/UserService/src/main/java/com/amol/user/service/entities/Rating.java
+++ microservices/01-demo-project/UserService/src/main/java/com/amol/user/service/entities/Rating.java
@@ -14,4 +14,5 @@ public class Rating {
     private float rating;
     private String remark;
     private String feedback;
+    private Hotel hotel;
 }
--- microservices/01-demo-project/UserService/src/main/java/com/amol/user/service/services/impl/UserServiceImpl.java
+++ microservices/01-demo-project/UserService/src/main/java/com/amol/user/service/services/impl/UserServiceImpl.java
@@ -1,5 +1,7 @@
 package com.amol.user.service.services.impl;
 
+import com.amol.user.service.entities.Hotel;
+import com.amol.user.service.entities.Rating;
 import com.amol.user.service.entities.User;
 import com.amol.user.service.exceptions.ResourceNotFoundException;
 import com.amol.user.service.repositories.UserRepository;
@@ -8,16 +10,17 @@ import com.amol.user.service.services.UserService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @Service
 public class UserServiceImpl implements UserService {
-
 
     @Autowired
     private UserRepository userRepo;
@@ -62,11 +65,27 @@ public class UserServiceImpl implements UserService {
         // fetch rating of the above user from RATING SERVICE
         // http://localhost:8083/ratings/user/66e07070-dd84-4071-a21e-9b8c7126676f
 
-        ArrayList ratingOfUserData = restTemplate.getForObject("http://localhost:8083/ratings/user/"+user.getUserId(), ArrayList.class);
-
+        Rating[] ratingOfUserData = restTemplate.getForObject("http://localhost:8083/ratings/user/"+user.getUserId(), Rating[].class);
         // logger.info() is used for audit purpose.
         logger.info("{} " ,ratingOfUserData);
-        user.setRatings(ratingOfUserData);
+
+        // convert array to ArrayList
+        List<Rating> ratings =  Arrays.stream(ratingOfUserData).toList();
+
+
+        List<Rating> ratingList = ratings.stream().map( rating -> {
+            // api call to hotel service to get the hotel
+            // http://localhost:8082/hotels/33a5b6dd-a557-4da7-9ad0-bf1325861246
+            ResponseEntity<Hotel> forEntity  = restTemplate.getForEntity("http://localhost:8082/hotels/"+rating.getHotelId(), Hotel.class);
+            Hotel hotel = forEntity.getBody();
+            logger.info("Response status code: {} ", forEntity.getStatusCode());
+            // set the hotel to rating
+            rating.setHotel(hotel);
+            //return rating
+            return rating;
+        }).collect(Collectors.toList());
+
+        user.setRatings(ratingList);
         return user;
     }
 
